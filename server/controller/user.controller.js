23c5f1229fc9fc90f31301afb63c67aed2111a1e
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-08-10 17:06:51 +0700

    create get user by id

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -87,9 +87,19 @@ const loginUser = async (req, res) => {
 const getAllUser = async (req, res) => {
   try {
     const users = await User.find();
-    res.status(200).json({ users, success: true });
+    res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: error.message, success: false });
   }
 };
-module.exports = { registerUser, loginUser, getAllUser };
+
+const getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message, success: false });
+  }
+};
+module.exports = { registerUser, loginUser, getAllUser, getUserById };
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { registerUser, loginUser, getAllUser } = require("../controller/user.controller");
+const { registerUser, loginUser, getAllUser, getUserById } = require("../controller/user.controller");
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/users", getAllUser);
+router.get("/user/:id", getUserById);
 module.exports = router;
