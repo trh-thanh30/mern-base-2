4ec2c3a7c1d6b3710bd8d44b66657d51816a997f
trh-thanh30 <tranhuuthanhcp@gmail.com>
2024-08-12 17:21:08 +0700

    create brand route

--- server/controller/brand.controller.js
+++ server/controller/brand.controller.js
@@ -21,5 +21,60 @@ const createBrand = async (req, res) => {
     res.status(500).json({ message: error.message, success: false });
   }
 };
+const getAllBrand = async (req, res) => {
+  try {
+    const brands = await Brand.find();
+    res.status(200).json(brands);
+  } catch (error) {
+    res.status(500).json({ message: error.message, success: false });
+  }
+};
+const updateBrand = async (req, res) => {
+  const { role } = req.user;
+  if (role !== "admin") {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized user admin", success: false });
+  }
+  const { id } = req.params;
+  try {
+    const updateBrand = await Brand.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(200).json(updateBrand);
+  } catch (error) {
+    res.status(500).json({ message: error.message, success: false });
+  }
+};
+const deleteBrand = async (req, res) => {
+  const { role } = req.user;
+  if (role !== "admin") {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized user admin", success: false });
+  }
+  const { id } = req.params;
+  try {
+    const deleteBrand = await Brand.findByIdAndDelete(id);
+    res.status(200).json(deleteBrand);
+  } catch (error) {
+    res.status(500).json({ message: error.message, success: false });
+  }
+};
 
-module.exports = { createBrand };
+const getBrand = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const brand = await Brand.findById(id);
+    res.status(200).json(brand);
+  } catch (error) {
+    res.status(500).json({ message: error.message, success: false });
+  }
+};
+module.exports = {
+  createBrand,
+  getAllBrand,
+  updateBrand,
+  deleteBrand,
+  getBrand,
+};
--- server/routes/brand.route.js
+++ server/routes/brand.route.js
@@ -1,6 +1,17 @@
 const express = require("express");
-const { createBrand } = require("../controller/brand.controller");
+const {
+  createBrand,
+  updateBrand,
+  deleteBrand,
+  getAllBrand,
+  getBrand,
+} = require("../controller/brand.controller");
 const verifyToken = require("../middlewares/verifyToken");
 const router = express.Router();
 router.post("/", verifyToken, createBrand);
+router.put("/:id", verifyToken, updateBrand);
+router.delete("/:id", verifyToken, deleteBrand);
+router.get("/:id", getBrand);
+router.get("/", getAllBrand);
+
 module.exports = router;
